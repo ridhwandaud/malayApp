7daa0a6d7c1ba864d4281f4c763b6cabec38af21
ridhwandaud <m.ridhwan.daud@gmail.com>
2017-08-26 18:07:50 +0800

    change to view pager

---
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CategoryAdapter extends FragmentPagerAdapter {
+
+    public CategoryAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new ColorsFragment();
+        } else if (position == 1){
+            return new NumbersFragment();
+        }else if (position == 2){
+            return new NumbersFragment();
+        }else {
+            return new PhrasesFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
